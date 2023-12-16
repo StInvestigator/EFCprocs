904830d013255536d7e125edf18b60c4cd6640b4
Murashko Vladislav <vladislavuh2000@gmail.com>
2023-12-16 17:50:27 +0200

    hometask done

--- Program.cs
+++ Program.cs
@@ -1,7 +1,35 @@
-﻿internal class Program
+﻿using Microsoft.Data.SqlClient;
+using Mirgation06_12;
+
+internal class Program
 {
     static void Main(string[] args)
     {
-            
-    }
+        try
+        {
+            using (GameContext db = new GameContext())
+            {
+                //db.Top3StudiosByGames();
+                //db.Top1StudiosByGames();
+                //db.Top3StylesByGames();
+                //db.Top1StylesByGames();
+                //db.Top3StylesBySoldCopies();
+                //db.Top1StylesBySoldCopies();
+                //db.Top3SinglePlayers();
+                //db.Top1SinglePlayer();
+                //db.Top3MultiPlayers();
+                //db.Top1MultiPlayer();
+                //db.Top1Game();
+                db.DeleteGamesWithLessSoldCopies(600);
+            }
+        }
+        catch (SqlException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    } 
 }
